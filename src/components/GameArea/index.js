ee6f10c3ea471f7da7eeeb07d8974dcae214d969
Lio <lionell.marquez@gmail.com>
2019-08-21 20:55:08 -0400

    All images added and made fluid

--- src/components/GameArea/index.js
+++ src/components/GameArea/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import imageList from '../../imageList.json'
 import ImgCard from '../ImgCard'
-import imageFolder from '../../assets/images/showcase.jpeg'
 import './style.css'
-// import imageFolder from 'src/assets/images/showcase.jpeg'
 
 class GameArea extends React.Component {
     state = {
@@ -14,7 +12,6 @@ class GameArea extends React.Component {
     return(
         <div className="wrapper">
           <div className="card-columns">
-            {console.log(imageFolder)}
             {this.state.imageList.map(image => (
             <ImgCard 
               id={image.id}
--- src/components/ImgCard/index.js
+++ src/components/ImgCard/index.js
@@ -1,14 +1,12 @@
 import React from "react";
-import "./style.css";
 
-// the relative path "{require('../../assets/images/showcase.jpeg')}" works here, but not when referenced in the JSON file.
 function ImgCard(props) {
     const imageClick = ()=> {
         console.log('Clicked ImgCard')
     }
     return(
-        <div className="card">
-            <img src={props.image} alt={props.id} onClick={()=>imageClick()}/>
+        <div className="card text-center">
+            <img className="img-fluid" src={props.image} alt={props.id} onClick={()=>imageClick()}/>
             {console.error()}
         </div>
     )
